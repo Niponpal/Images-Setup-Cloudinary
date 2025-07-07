be79f83bfab3634b79c2ba9ea7ecd310bf986785
Niponpal <niponpal52@gmail.com>
2025-07-07 23:20:48 +0600

    Done

--- ImagesSetup/obj/Debug/net8.0/ImagesSetup.AssemblyInfo.cs
+++ ImagesSetup/obj/Debug/net8.0/ImagesSetup.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImagesSetup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f77a77f161c3898d9df402680d7c242641554d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImagesSetup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImagesSetup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
